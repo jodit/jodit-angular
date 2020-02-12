4f645e7a5d914677b1167f8522c518386b65d145
bob <foo@bar.com>
2020-02-12 14:03:16 +0200

    Ignore output on prefix

--- projects/jodit-angular-lib/src/lib/Events.ts
+++ projects/jodit-angular-lib/src/lib/Events.ts
@@ -1,41 +1,42 @@
-import { Output, EventEmitter } from '@angular/core';
+import {Output, EventEmitter} from '@angular/core';
 
 export interface EventObj {
-    args: any[];
-    editor: any;
+  args: any[];
+  editor: any;
 }
 
 export class Events {
-    @Output() onChange: EventEmitter<EventObj> = new EventEmitter();
-    @Output() onBeforeEnter: EventEmitter<EventObj> = new EventEmitter(false);
-    @Output() onKeydown: EventEmitter<EventObj> = new EventEmitter(false);
-    @Output() onMousedown: EventEmitter<EventObj> = new EventEmitter(false);
-    @Output() onClick: EventEmitter<EventObj> = new EventEmitter(false);
-    @Output() onFocus: EventEmitter<EventObj> = new EventEmitter();
-    @Output() onBlur: EventEmitter<EventObj> = new EventEmitter();
-    @Output() onPaste: EventEmitter<EventObj> = new EventEmitter(false);
-    @Output() onResize: EventEmitter<EventObj> = new EventEmitter();
-    @Output() onBeforeCommand: EventEmitter<EventObj> = new EventEmitter(false);
-    @Output() onAfterCommand: EventEmitter<EventObj> = new EventEmitter();
-    @Output() onAfterExec: EventEmitter<EventObj> = new EventEmitter();
-    @Output() onAfterPaste: EventEmitter<EventObj> = new EventEmitter();
-    @Output() onChangeSelection: EventEmitter<EventObj> = new EventEmitter();
+  // tslint:disable:no-output-on-prefix
+  @Output() onChange: EventEmitter<EventObj> = new EventEmitter();
+  @Output() onBeforeEnter: EventEmitter<EventObj> = new EventEmitter(false);
+  @Output() onKeydown: EventEmitter<EventObj> = new EventEmitter(false);
+  @Output() onMousedown: EventEmitter<EventObj> = new EventEmitter(false);
+  @Output() onClick: EventEmitter<EventObj> = new EventEmitter(false);
+  @Output() onFocus: EventEmitter<EventObj> = new EventEmitter();
+  @Output() onBlur: EventEmitter<EventObj> = new EventEmitter();
+  @Output() onPaste: EventEmitter<EventObj> = new EventEmitter(false);
+  @Output() onResize: EventEmitter<EventObj> = new EventEmitter();
+  @Output() onBeforeCommand: EventEmitter<EventObj> = new EventEmitter(false);
+  @Output() onAfterCommand: EventEmitter<EventObj> = new EventEmitter();
+  @Output() onAfterExec: EventEmitter<EventObj> = new EventEmitter();
+  @Output() onAfterPaste: EventEmitter<EventObj> = new EventEmitter();
+  @Output() onChangeSelection: EventEmitter<EventObj> = new EventEmitter();
 }
 
 
 export const validEvents: (keyof Events)[] = [
-    'onChange',
-    'onBeforeEnter',
-    'onKeydown',
-    'onMousedown',
-    'onClick',
-    'onFocus',
-    'onBlur',
-    'onPaste',
-    'onResize',
-    'onBeforeCommand',
-    'onAfterCommand',
-    'onAfterExec',
-    'onAfterPaste',
-    'onChangeSelection',
+  'onChange',
+  'onBeforeEnter',
+  'onKeydown',
+  'onMousedown',
+  'onClick',
+  'onFocus',
+  'onBlur',
+  'onPaste',
+  'onResize',
+  'onBeforeCommand',
+  'onAfterCommand',
+  'onAfterExec',
+  'onAfterPaste',
+  'onChangeSelection',
 ];
