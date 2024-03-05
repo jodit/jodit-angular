4fe7ee0efd11fc7b0d8aae5b323b6db0ac481eae
kashgurung <b.bikashgurung@outlook.com>
2024-03-05 17:44:07 +0600

    update to angular 11

--- projects/jodit-angular-app/src/app/app.component.spec.ts
+++ projects/jodit-angular-app/src/app/app.component.spec.ts
@@ -1,10 +1,10 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 import { JoditAngularModule } from 'jodit-angular';
 
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [
         JoditAngularModule
@@ -14,12 +14,12 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
+  it('should create the app', waitForAsync(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should be display the Jodit html editor`, async(() => {
+  it(`should be display the Jodit html editor`, waitForAsync(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
--- projects/jodit-angular-lib/src/lib/jodit-angular.component.spec.ts
+++ projects/jodit-angular-lib/src/lib/jodit-angular.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { JoditAngularComponent } from './jodit-angular.component';
 
@@ -6,7 +6,7 @@ describe('JoditAngularComponent', () => {
   let component: JoditAngularComponent;
   let fixture: ComponentFixture<JoditAngularComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ JoditAngularComponent ]
     })
